d97338c4a49802273f97e8fb26a3fd9ac66f7569
Lynn Gabbay <gabbay@gmail.com>
2015-03-27 05:25:26 -0700

    added skeleton for videojs plugin

---
+++ videojs.ima_flash.js
@@ -0,0 +1,19 @@
+/**
+ * Video.JS plugin for Flash IMA 
+ */
+(function(window, document, videojs, undefined) {
+	"use strict";
+
+	/**
+	 * Register the ad integration plugin.
+	 * To initialize for a player, call player.ima_flash(options).
+	 *
+	 * @param {mixed} options Hash of options for the ima_flash plugin.
+	 */
+	videojs.plugin("ima_flash", function(options) {
+
+		var player = this;
+
+	});
+
+})(window, document, videojs);
